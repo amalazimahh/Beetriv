afaa44307ac4e2e917e492fc1732501c720cf879
Haziq Zulhazmi <19ftt1099@student.pb.edu.bn>
2021-08-09 12:52:57 +0800

    #1 Email Verification

---
+++ emailverifcation.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Beetriv - Email Verification</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                    <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+                    <div class="col-lg-7">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Welcome to Beetriv!</h1>
+                            </div>
+
+                            <div class="text-center">
+                                <p class="h4 text-gray-900 mb-4">We just sent a confirmation code over to email@email.com</p>
+                            </div>
+
+                            <form class="user">
+                                <div class="form-group row">
+                                    
+                                </div>
+                                
+                                <div class="text-center">
+                                        <input type="text" class="form-control form-control-user" id="vcode" placeholder="Verification Code" >
+                                    </div>
+                                
+                                <div class="form-group row">
+                                    
+                                    
+                                </div>
+                                <a href="login.html" class="btn btn-primary btn-user btn-block">
+                                    Log In
+                                </a>
+                                <hr>
+                                
+                                <div class="text-center">
+                                <a class="small" href="forgot-password.html">Didn't work? Send me another code.</a>
+                            </div>
+                            </form>
+                            <hr>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
