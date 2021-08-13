52f107a00de235374eb78ef1368cb9a03c6cacda
amalazimah <19FTT1205@student.pb.edu.bn>
2021-08-14 02:14:57 +0800

    created succes payment page for paypal checkout

---
+++ success-payment.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payment Successful</title>
+</head>
+<body>
+    <div id="success-logo">
+        <img src="assets/paid.png" alt="">
+    </div>
+
+    <h4>Successful !</h4>
+    <p>Your order has been placed with an ID :</p>
+</body>
+</html>
