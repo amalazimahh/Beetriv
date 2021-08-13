63debf09d32f6fefff52478d9240927199206846
amalazimah <19FTT1205@student.pb.edu.bn>
2021-08-13 15:32:17 +0800

    added paypal javascript

--- paypal-payment.php
+++ paypal-payment.php
@@ -70,6 +70,30 @@
 
 
     <script src="https://www.paypal.com/sdk/js?client-id=AUr--h5puQ-3ZYpriY7iae2SKlZREKxlTVYyTUMWUH6mJnQGn9Upc6Fcf6bRbCgqlFi8v83Yu26Z_XEW"></script>
-    <script src="index.js"></script>
+    <script> 
+        paypal.Buttons({
+        style : {
+        color: 'blue',
+        shape: 'pill'
+        },
+         createOrder: function (data, actions) {
+            return actions.order.create({
+                purchase_units : [{
+                    amount: {
+                        value: '0.1'
+                    }
+                }]
+            });
+        },
+        onApprove: function (data, actions) {
+            return actions.order.capture().then(function (details) {
+             console.log(details)
+                window.location.replace("http://localhost:63342/tutorial/paypal/success.php")
+            })
+        },
+        onCancel: function (data) {
+            window.location.replace("http://localhost:63342/tutorial/paypal/Oncancel.php")
+        }
+    }).render('#paypal-payment-button');</script>
 </body>
 </html>
