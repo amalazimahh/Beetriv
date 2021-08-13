594adc2990a270a8226f10cf3deb483e2064c02c
amalazimah <19FTT1205@student.pb.edu.bn>
2021-08-14 02:16:14 +0800

    created unsuccessful paypal payment page

---
+++ cancel-payment.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payment Rejected</title>
+</head>
+<body>
+    <div id="fail-logo">
+        <img src="img/warning.png" alt="">
+    </div>
+
+    <h4>Oops! Something went wrong.</h4>
+    <p>Payment has been cancelled.</p>
+</body>
+</html>
