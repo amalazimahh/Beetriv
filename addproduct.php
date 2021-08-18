45ec119b2cf695b81b47e5aa01356fe21be955cd
Fiqah Zuhairie <19FTT1162@student.pb.edu.bn>
2021-08-18 21:06:59 +0800

    added bootstrap link for nav

--- addproduct.php
+++ addproduct.php
@@ -18,6 +18,8 @@ require_once "connection.php";
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/user-profile.css">
     <style>
 * {
   box-sizing: border-box;
@@ -86,7 +88,7 @@ input[type=submit]:hover {
 }
 
 .add-prd {
-        margin-top: 15px;
+        padding-top: 5px;
         margin-bottom: 20px;
         margin-left: 80px;
         padding-left: 20px;
