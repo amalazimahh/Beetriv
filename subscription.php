d79cb471e407abd793090f3520ced211a16bb2e1
Fiqah Zuhairie <19FTT1162@student.pb.edu.bn>
2021-08-29 13:06:58 +0800

    justify content centent

--- subscription.php
+++ subscription.php
@@ -63,7 +63,7 @@
 
 <section class="pricing py-5">
   <div class="container">
-    <div class="row">
+    <div class="row justify-content-center">
       <!-- Free Tier -->
       <div class="col-lg-4">
         <div class="card mb-5 mb-lg-0">
@@ -91,35 +91,12 @@
           </div>
         </div>
       </div>
-      <!-- Plus Tier -->
-      <div class="col-lg-4">
-        <div class="card mb-5 mb-lg-0">
-          <div class="card-body">
-            <h5 class="card-title text-muted text-uppercase text-center">Vendor</h5>
-            <h6 class="card-price text-center">$9<span class="period">/month</span></h6>
-            <hr>
-            <ul class="fa-ul">
-              <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>5 Users</strong></li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>50GB Storage</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Free Subdomain</li>
-              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
-                Reports</li>
-            </ul>
-            <div class="d-grid">
-              <a href="#" class="btn btn-warning text-uppercase">Upgrade</a>
-            </div>
-          </div>
-        </div>
-      </div>
+  
       <!-- Pro Tier -->
       <div class="col-lg-4">
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title text-muted text-uppercase text-center">Runner</h5>
+            <h5 class="card-title text-muted text-uppercase text-center">Vendor</h5>
             <h6 class="card-price text-center">$49<span class="period">/month</span></h6>
             <hr>
             <ul class="fa-ul">
