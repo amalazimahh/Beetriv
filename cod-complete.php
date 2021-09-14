08baae8b3094738a4184e2ab636c4020c2430772
Yusnadi Yudin <19FTT1119@student.pb.edu.bn>
2021-09-14 10:21:10 +0800

    cod completed

---
+++ cod-complete.php
@@ -0,0 +1,281 @@
+<?php
+ob_start();
+session_start();
+
+require_once "connection.php";
+$email = $_SESSION['email'];
+// echo $email;
+
+unset($_SESSION['cart_items']);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href=" https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+  <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="css/footer.css">
+  <script language="javascript" src="js/calendar.js"></script>
+  <title>Checkout - Place Order</title>
+  <style>
+    .checkout-progress{
+      margin: 80px auto;
+    }
+
+    ul{
+      text-align: center;
+    }
+    
+    ul li{
+      display: inline-block;
+      width: 200px;
+      position: relative;
+      
+    }
+
+    ul li .fas{
+        background: #000;
+        /* width: 8px;
+        height: 8px;  */
+        color: #fff;
+        border-radius: 50%;
+        padding: 6px;
+    }
+
+    ul li .fas::after{
+      content: '';
+      background: #ccc;
+      height: 7px;
+      width: 250px;
+      display: block;
+      position: absolute;
+      left: 0;
+      top: 10px;
+      z-index: -1;
+    }
+
+    ul li:nth-child(1) .fas{
+      background: #148e14;
+    }
+
+    ul li:nth-child(1) .fas::after{
+      background: #148e14;
+      
+    }
+
+    ul li:first-child .fas::after{
+      width: 105px;
+      left: 100px;
+    }
+
+    ul li:last-child .fas::after{
+      width: 105px;
+    }
+
+    .checkout-form{
+      border-radius: 5px;
+      padding: 20px;
+    }
+
+    .checkout-btn{
+      text-align: center;
+    }
+
+    button{
+      text-align: center;
+      padding: 20px;
+      background-color: #000;
+      color: #fff;
+      text-transform: capitalize;
+      border-radius: 10px;
+      border: #000;
+    }
+
+    .checkout-btn a{
+      text-decoration: none;
+      color: #fff;
+    }
+    * {box-sizing: border-box;}
+ul {list-style-type: none;}
+body {font-family: Verdana, sans-serif;}
+
+.month {
+  padding: 70px 25px;
+  width: 100%;
+  background: #1abc9c;
+  text-align: center;
+}
+
+.month ul {
+  margin: 0;
+  padding: 0;
+}
+
+.month ul li {
+  color: white;
+  font-size: 20px;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+}
+
+.month .prev {
+  float: left;
+  padding-top: 10px;
+}
+
+.month .next {
+  float: right;
+  padding-top: 10px;
+}
+
+.weekdays {
+  margin: 0;
+  padding: 10px 0;
+  background-color: #ddd;
+}
+
+.weekdays li {
+  display: inline-block;
+  width: 13.6%;
+  color: #666;
+  text-align: center;
+}
+
+.days {
+  padding: 10px 0;
+  background: #eee;
+  margin: 0;
+}
+
+.days li {
+  list-style-type: none;
+  display: inline-block;
+  width: 13.6%;
+  text-align: center;
+  margin-bottom: 5px;
+  font-size:12px;
+  color: #777;
+}
+
+.days li .active {
+  padding: 5px;
+  background: #1abc9c;
+  color: white !important
+}
+.days li .nonactive {
+  padding: 5px;
+  background: #FF2E2E;
+  color: white !important
+}
+
+/* Add media queries for smaller screens */
+@media screen and (max-width:720px) {
+  .weekdays li, .days li {width: 13.1%;}
+}
+
+@media screen and (max-width: 420px) {
+  .weekdays li, .days li {width: 12.5%;}
+  .days li .active {padding: 2px;}
+}
+
+@media screen and (max-width: 290px) {
+  .weekdays li, .days li {width: 12.2%;}
+}
+  </style>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#!">Beetriv</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link" aria-current="page" href="store.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#!">All Products</a></li>
+                                <li><hr class="dropdown-divider" /></li>
+                                <li><a class="dropdown-item" href="#!">Popular Items</a></li>
+                                <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <a class="nav-item nav-link" style='color:black' aria-current="page" href="cart.php">
+                    <i class="bi bi-cart4" style='color:black'><?php echo (isset($_SESSION['cart_items']) && count($_SESSION['cart_items'])) > 0 ? count($_SESSION['cart_items']):''; ?></i>
+                    </a>
+                </div>
+            </div>
+        </nav>
+      <section class="container px-4 px-lg-5 my-5" >
+        <?php if(isset($_SESSION['cart_items']) && count($_SESSION['cart_items']) > 0){?>
+            <?php }?>
+
+            <h3>Your orders has been completed!</h3>
+            <h5>Your Items will be Deliver on 10th August 2021.</h5>
+            <p>A Runner will contact you As Soon As Possible. Thank You for using Beetriv!!</p>
+
+
+            
+        </section>
+        <!-- Footer-->
+        <footer class="site-footer">
+
+            <div class="container">
+                <div class="row">
+                <!-- first section -->
+                <div class="col-xs-6 col-md-3">
+                    <h6>CORPORATE</h6>
+                    <ul class="footer-links">
+                        <li><a href="footer/about.php">About Beetriv</a></li>
+                        <li><a href="footer/privacy-policy.php">Privacy Policy</a></li>
+                        <li><a href="footer/termsco.php">Terms and Conditions</a></li>
+                    </ul>
+                </div>
+
+                <!-- second section -->
+                <div class="col-xs-6 col-md-3">
+                    <h6>DEALS, PAYMENT & DELIVERY</h6>
+                    <ul class="footer-links">
+                        <li><a href="footer/deals.php">Our Deals</a></li>
+                        <li><a href="footer/delivery.php">Delivery Services</a></li>
+                        <li><a href="footer/payment.php">Payment</a></li>
+                    </ul>
+                </div>
+
+                <!-- third section -->
+                <div class="col-xs-6 col-md-3">
+                    <h6>CUSTOMER CARE</h6>
+                    <ul class="footer-links">
+                        <li><a href="footer/be-seller.php">Become Our Seller</a></li>
+                        <li><a href="footer/buy-guides.php">How to Buy on Beetriv</a></li>
+                        <li><a href="footer/sell-guides.php">How to Sell on Beetriv</a></li>
+                        <li><a href="footer/bid-guides.php">How Bidding Works</a></li>
+                        <li><a href="footer/customer-protection.php">Customer Protection</a></li>
+                        <li><a href="footer/faq.php">FAQ</a></li>
+                    </ul>
+                </div>
+
+                <!-- fourth section -->
+                <div class="col-xs-6 col-md-3">
+                    <h6>CONTACT US</h6>
+                    <p>Phone: 257 3663</p>
+                    <p>Email: beetrivteam@gmail.com</p>
+                    <p>Instagram: @beetriv</p>
+                    <p>Facebook: @beetriv</p>
+                </div>
+              </div>
+            </div>
+        </footer>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
