96a32329bf3cc83fc6b309522f6eb89a8a0d5225
amalazimah <19FTT1205@student.pb.edu.bn>
2021-10-08 23:41:20 +0800

    modified purchase page

--- purchase.php
+++ purchase.php
@@ -4,12 +4,12 @@
     $email = $_SESSION['email'];
     // echo $email;
 
-    if(isset($_GET['action'],$_GET['item']) && $_GET['action'] == 'remove')
-    {
-        unset($_SESSION['cart_items'][$_GET['item']]);
-        header('location:cart.php');
-        exit();
-    }
+    // if(isset($_GET['action'],$_GET['item']) && $_GET['action'] == 'remove')
+    // {
+    //     unset($_SESSION['cart_items'][$_GET['item']]);
+    //     header('location:cart.php');
+    //     exit();
+    // }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -69,91 +69,76 @@
                 </div>
             </div>
         </nav>
-        
-        <!-- Product section-->
-        <!-- Insert some code to see if order history exist, if yes return true, else display Purchases empty -->
-    <!-- <div class="row mt-3">
-     <div class="col-md-12">
-         <section class="container px-4 px-lg-5 my-5" >
-         <a href="cart.php">Your Shopping Cart |</a>
-         <a href="purchase.php">Your Purchases</a>
-        <?php //if(empty($_SESSION['cart_items'])){?>
-        <table class="table">
-            <tr>
-                <td>
-                    <p>Your cart is empty</p>
-                </td>
-            </tr>
-        </table> -->
-        <!-- <?php //}?> -->
-        <!-- <?php //if(isset($_SESSION['cart_items']) && count($_SESSION['cart_items']) > 0){?>
-        <table class="table">
-           <thead>
-                <tr>
-                    <th>Product</th>
-                    <th>Price</th>
-                    <th>Qty</th>
-                    <th>Total</th>
-                </tr>
-            </thead>
-            <tbody> -->
-                <?php 
-                    // $totalCounter = 0;
-                    // $itemCounter = 0;
-                    // foreach($_SESSION['cart_items'] as $key => $item){ 
 
-                    // $img = $item['product_img'];
-                    
-                    // $total = $item['product_price'] * $item['qty'];
-                    // $totalCounter+= $total;
-                    // $itemCounter+=$item['qty'];
-                    ?>
-                    <!-- <tr>
-                        <td>
-                            <img src="data:image/jpg;charset=utf8;base64,<?php //echo base64_encode($img); ?>"class="rounded img-thumbnail mr-2" style="width:40px;">
-                            <?php //echo $item['product_name'];?>
-                            
-                            <a href="cart.php?action=remove&item=<?php //echo $key?>" class="text-danger">
-                                <i class="bi bi-trash-fill"></i>
-                            </a>
+            <div class="row mt-3">
+                <div class="col-md-12">
+                    <section class="container px-4 px-lg-5 my-5" >
+                        <!-- Show lists of items purchased -->
+                        <!-- <table class="table">
+                            <tr>
+                                <td>
+                                    <p>Your puchases is empty. Shop now !</p>
+                                </td>
+                            </tr>
+                        </table> -->
 
-                        </td>
-                        <td>
-                            $<?php //echo $item['product_price'];?>
-                        </td>
-                        <td>
-                            <?php //echo $item['qty'];?>
-                        </td>
-                        <td>
-                            <?php //echo $total;?>
-                        </td>
-                    </tr> -->
-                <?php //}?>
-                <!-- <tr class="border-top border-bottom">
-                    <td></td>
-                    <td></td>
-                    <td>
-                        <strong> -->
-                            <?php 
-                                //echo ($itemCounter==1)?$itemCounter.' item':$itemCounter.' items'; ?>
-                        <!-- </strong>
-                    </td> -->
-                    <!-- <td><strong>$<?php //echo $totalCounter;?></strong></td> -->
-                <!-- </tr> 
-                </tr>
-            </tbody> 
-        </table> -->
-        <!-- <div class="row">
-            <div class="col-md-11">
-				<a href="place-order.php">
-					<button class="btn btn-warning btn-lg float-right">Checkout</button>
-				</a>
+                        <table class="table">
+                        <thead>
+                                <tr>
+                                    <th>Product</th>
+                                    <th>Shop</th>
+                                    <th>Price</th>
+                                    <th>Qty</th>
+                                    <th>Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                    <tr>
+                                        <td>
+                                            <img src="data:image/jpg;charset=utf8;base64" class="rounded img-thumbnail mr-2" style="width:40px;">
+                                            <p>Example</p>
+                                        </td>
+                                        <td>
+                                            [Vendors]
+                                        </td>
+                                        <td>
+                                            $12.00
+                                        </td>
+                                        <td>
+                                            1
+                                        </td>
+                                        <td>
+                                            $12.00
+                                        </td>
+                                        <td>
+                                            <button class="btn btn-outline-dark flex-shrink-0">
+                                                <a class="nav-item nav-link" style='color:black' aria-current="page" href="seller-review.php">
+                                                    <i class="bi bi-pen-fill"></i>
+                                                    Review
+                                                </a>
+                                                
+                                            </button>
+                                        </td>
+                                    </tr>
+                                <!-- <tr class="border-top border-bottom">
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>
+                                        <strong>
+                                        
+                                        </strong>
+                                    </td>
+                                </tr>  -->
+                                </tr>
+                            </tbody> 
+                        </table>
+
+                    </section>
+                </div>
             </div>
-        </div> -->
+            <br><br><br><br><br><br><br><br>
         
-            <?php //}?>
-        <!-- </section> -->
-
         <!-- Footer-->
         <footer class="site-footer">
 
