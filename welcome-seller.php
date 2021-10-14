70cfb32157cf4114e7f158095f536b6206f5187c
Fiqah Zuhairie <19FTT1162@student.pb.edu.bn>
2021-10-14 18:42:04 +0800

    disables seller when expired

--- welcome-seller.php
+++ welcome-seller.php
@@ -5,8 +5,11 @@ require_once "connection.php";
 
 $email = $_SESSION['email'];
 
+//notify when time expired
+date_default_timezone_set('Asia/Brunei');
+$time_register = date('Y-m-d H:i:s');
 
-$updte = $conn->query("UPDATE users SET type='seller' WHERE email='$email'");
+$updte = $conn->query("UPDATE users SET type='seller', seller_register='$time_register', seller_period='active' WHERE email='$email'");
 header ('location: subscription.php');
 
 ?>
