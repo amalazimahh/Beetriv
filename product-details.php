e1a79355f5b63d4af80d4bb949df76eb4d8ff463
amalazimah <19FTT1205@student.pb.edu.bn>
2021-10-15 15:04:38 +0800

    changed PHPMailer script to beetriv email

--- product-details.php
+++ product-details.php
@@ -298,26 +298,26 @@ else
                     $mail->isSMTP();
     
                     //Set the SMTP server 
-                    $mail->Host = 'smtp.gmail.com';
-    
+                    $mail->Host = 'mail.beetriv.com';
+
                     //Enable SMTP authentication
                     $mail->SMTPAuth = true;
-    
+
                     //SMTP username
-                    $mail->Username = 'ayamketupat02@gmail.com';
-    
+                    $mail->Username = 'admin@beetriv.com';
+
                     //SMTP password
-                    $mail->Password = 'k4k5dpkk';
-    
+                    $mail->Password = '4bx~~ZJ8HJyq';
+
                     //SMTP username
-                    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    
+                    $mail->SMTPSecure = 'ssl';
+
                     //SMTP PORT
-                    $mail->Port = 587;
-    
+                    $mail->Port = '290';
+
                     //Recipients
-                    $mail->setFrom('haziqzulhazmi@gmail.com','beetriv.com');
-    
+                    $mail->setFrom('admin@beetriv.com','Admin Beetriv');
+        
                     //add recipient
                     $mail->addAddress($current_bidder,$username);
     
@@ -353,25 +353,25 @@ else
                     $mail->isSMTP();
     
                     //Set the SMTP server 
-                    $mail->Host = 'smtp.gmail.com';
-    
+                    $mail->Host = 'mail.beetriv.com';
+
                     //Enable SMTP authentication
                     $mail->SMTPAuth = true;
-    
+
                     //SMTP username
-                    $mail->Username = 'ayamketupat02@gmail.com';
-    
+                    $mail->Username = 'admin@beetriv.com';
+
                     //SMTP password
-                    $mail->Password = 'k4k5dpkk';
-    
+                    $mail->Password = '4bx~~ZJ8HJyq';
+
                     //SMTP username
-                    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    
+                    $mail->SMTPSecure = 'ssl';
+
                     //SMTP PORT
-                    $mail->Port = 587;
-    
+                    $mail->Port = '290';
+
                     //Recipients
-                    $mail->setFrom('haziqzulhazmi@gmail.com','beetriv.com');
+                    $mail->setFrom('admin@beetriv.com','Admin Beetriv');
     
                     //add recipient
                     $mail->addAddress($seller,$username);
@@ -424,25 +424,25 @@ else
                 $mail->isSMTP();
 
                 //Set the SMTP server 
-                $mail->Host = 'smtp.gmail.com';
+                $mail->Host = 'mail.beetriv.com';
 
                 //Enable SMTP authentication
                 $mail->SMTPAuth = true;
 
                 //SMTP username
-                $mail->Username = 'ayamketupat02@gmail.com';
+                $mail->Username = 'admin@beetriv.com';
 
                 //SMTP password
-                $mail->Password = 'k4k5dpkk';
+                $mail->Password = '4bx~~ZJ8HJyq';
 
                 //SMTP username
-                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                $mail->SMTPSecure = 'ssl';
 
                 //SMTP PORT
-                $mail->Port = 587;
+                $mail->Port = '290';
 
                 //Recipients
-                $mail->setFrom('haziqzulhazmi@gmail.com','beetriv.com');
+                $mail->setFrom('admin@beetriv.com','Admin Beetriv');
 
                 //add recipient
                 $mail->addAddress($email,$username);
@@ -490,25 +490,25 @@ else
                 $mail->isSMTP();
 
                 //Set the SMTP server 
-                $mail->Host = 'smtp.gmail.com';
+                $mail->Host = 'mail.beetriv.com';
 
                 //Enable SMTP authentication
                 $mail->SMTPAuth = true;
 
                 //SMTP username
-                $mail->Username = 'ayamketupat02@gmail.com';
+                $mail->Username = 'admin@beetriv.com';
 
                 //SMTP password
-                $mail->Password = 'k4k5dpkk';
+                $mail->Password = '4bx~~ZJ8HJyq';
 
                 //SMTP username
-                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                $mail->SMTPSecure = 'ssl';
 
                 //SMTP PORT
-                $mail->Port = 587;
+                $mail->Port = '290';
 
                 //Recipients
-                $mail->setFrom('haziqzulhazmi@gmail.com','beetriv.com');
+                $mail->setFrom('admin@beetriv.com','Admin Beetriv');
 
                 //add recipient
                 $mail->addAddress($seller,$username);
@@ -573,25 +573,25 @@ else
                 $mail->isSMTP();
 
                 //Set the SMTP server 
-                $mail->Host = 'smtp.gmail.com';
+                $mail->Host = 'mail.beetriv.com';
 
                 //Enable SMTP authentication
                 $mail->SMTPAuth = true;
 
                 //SMTP username
-                $mail->Username = 'ayamketupat02@gmail.com';
+                $mail->Username = 'admin@beetriv.com';
 
                 //SMTP password
-                $mail->Password = 'k4k5dpkk';
+                $mail->Password = '4bx~~ZJ8HJyq';
 
                 //SMTP username
-                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                $mail->SMTPSecure = 'ssl';
 
                 //SMTP PORT
-                $mail->Port = 587;
+                $mail->Port = '290';
 
                 //Recipients
-                $mail->setFrom('haziqzulhazmi@gmail.com','beetriv.com');
+                $mail->setFrom('admin@beetriv.com','Admin Beetriv');
 
                 //add recipient
                 $mail->addAddress($current_bidder,$username);
@@ -630,25 +630,25 @@ else
                 $mail->isSMTP();
 
                 //Set the SMTP server 
-                $mail->Host = 'smtp.gmail.com';
+                $mail->Host = 'mail.beetriv.com';
 
                 //Enable SMTP authentication
                 $mail->SMTPAuth = true;
 
                 //SMTP username
-                $mail->Username = 'ayamketupat02@gmail.com';
+                $mail->Username = 'admin@beetriv.com';
 
                 //SMTP password
-                $mail->Password = 'k4k5dpkk';
+                $mail->Password = '4bx~~ZJ8HJyq';
 
                 //SMTP username
-                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                $mail->SMTPSecure = 'ssl';
 
                 //SMTP PORT
-                $mail->Port = 587;
+                $mail->Port = '290';
 
                 //Recipients
-                $mail->setFrom('haziqzulhazmi@gmail.com','beetriv.com');
+                $mail->setFrom('admin@beetriv.com','Admin Beetriv');
 
                 //add recipient
                 $mail->addAddress($seller,$username);
@@ -1138,112 +1138,7 @@ else
                     <?php }?>
 
         </section>
-        <!-- Related items section-->
-        <section class="py-5 bg-light">
-            <div class="container px-4 px-lg-5 mt-5">
-                <h2 class="fw-bolder mb-4">Related products</h2>
-                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                    <div class="col mb-5">
-                        <div class="card h-100">
-                            <!-- Product image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                            <!-- Product details-->
-                            <div class="card-body p-4">
-                                <div class="text-center">
-                                    <!-- Product name-->
-                                    <h5 class="fw-bolder">Fancy Product</h5>
-                                    <!-- Product price-->
-                                    $40.00 - $80.00
-                                </div>
-                            </div>
-                            <!-- Product actions-->
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col mb-5">
-                        <div class="card h-100">
-                            <!-- Sale badge-->
-                            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
-                            <!-- Product image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                            <!-- Product details-->
-                            <div class="card-body p-4">
-                                <div class="text-center">
-                                    <!-- Product name-->
-                                    <h5 class="fw-bolder">Special Item</h5>
-                                    <!-- Product reviews-->
-                                    <div class="d-flex justify-content-center small text-warning mb-2">
-                                        <div class="bi-star-fill"></div>
-                                        <div class="bi-star-fill"></div>
-                                        <div class="bi-star-fill"></div>
-                                        <div class="bi-star-fill"></div>
-                                        <div class="bi-star-fill"></div>
-                                    </div>
-                                    <!-- Product price-->
-                                    <span class="text-muted text-decoration-line-through">$20.00</span>
-                                    $18.00
-                                </div>
-                            </div>
-                            <!-- Product actions-->
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col mb-5">
-                        <div class="card h-100">
-                            <!-- Sale badge-->
-                            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
-                            <!-- Product image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                            <!-- Product details-->
-                            <div class="card-body p-4">
-                                <div class="text-center">
-                                    <!-- Product name-->
-                                    <h5 class="fw-bolder">Sale Item</h5>
-                                    <!-- Product price-->
-                                    <span class="text-muted text-decoration-line-through">$50.00</span>
-                                    $25.00
-                                </div>
-                            </div>
-                            <!-- Product actions-->
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col mb-5">
-                        <div class="card h-100">
-                            <!-- Product image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                            <!-- Product details-->
-                            <div class="card-body p-4">
-                                <div class="text-center">
-                                    <!-- Product name-->
-                                    <h5 class="fw-bolder">Popular Item</h5>
-                                    <!-- Product reviews-->
-                                    <div class="d-flex justify-content-center small text-warning mb-2">
-                                        <div class="bi-star-fill"></div>
-                                        <div class="bi-star-fill"></div>
-                                        <div class="bi-star-fill"></div>
-                                        <div class="bi-star-fill"></div>
-                                        <div class="bi-star-fill"></div>
-                                    </div>
-                                    <!-- Product price-->
-                                    $40.00
-                                </div>
-                            </div>
-                            <!-- Product actions-->
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        
 
         <!-- Footer-->
         <footer class="site-footer">
